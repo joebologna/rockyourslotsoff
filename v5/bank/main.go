6af96aa1c4cd05481e71e19d83480f9b15dfd34b
Joe Bologna <joebologna@mac.com>
2024-10-17 20:38:35 -0500

    Use echo to implement API

--- v5/bank/main.go
+++ v5/bank/main.go
@@ -2,92 +2,86 @@ package main
 
 import (
 	"encoding/binary"
-	"errors"
 	"log"
-	"math"
+	"net/http"
 	"strconv"
 
 	"github.com/dgraph-io/badger/v3"
-	"gofr.dev/pkg/gofr"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
-
-type Bank struct {
-	Id      int     `json:"id"`
-	Balance float32 `json:"balance"`
-}
 
 var db *badger.DB
 
 func main() {
+	var err error
+
 	// Open a BadgerDB database with logging disabled
 	opts := badger.DefaultOptions("./badgerdb").WithLogger(nil)
-	db, err := badger.Open(opts)
+	db, err = badger.Open(opts)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	// set initial balance
-	err = updateBalance(db, 100.0)
+	// Set initial balance
+	err = updateBalance(db, 100)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// get balance
-	balance, err := getBalance(db)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Balance:", balance)
+	// Initialize Echo
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	a := gofr.New()
+	// Define routes
+	e.GET("/balance", getBalanceHandler)
+	e.POST("/balance", updateBalanceHandler)
 
-	err = a.AddRESTHandlers(&Bank{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	a.Run()
+	// Start the server
+	e.Logger.Fatal(e.Start("127.0.0.1:9000"))
 }
 
-func (b *Bank) Get(c *gofr.Context) (interface{}, error) {
-	return getBalance(db)
-}
-
-func (b *Bank) Update(c *gofr.Context) (interface{}, error) {
-	bal := c.Param("balance")
-	if bal == "" {
-		return nil, errors.New("balance is required")
-	}
-	balance, err := strconv.ParseFloat(bal, 32)
-	if err != nil {
-		return nil, err
-	}
-	if err = updateBalance(db, float32(balance)); err != nil {
-		return nil, err
-	}
-	return balance, nil
-}
-
-func updateBalance(db *badger.DB, balance float32) (err error) {
-	err = db.Update(func(txn *badger.Txn) error {
+func updateBalance(db *badger.DB, balance int) error {
+	return db.Update(func(txn *badger.Txn) error {
 		balanceBytes := make([]byte, 4)
-		binary.LittleEndian.PutUint32(balanceBytes, math.Float32bits(balance))
+		binary.LittleEndian.PutUint32(balanceBytes, uint32(balance))
 		return txn.Set([]byte("balance"), balanceBytes)
 	})
-	return err
 }
 
-func getBalance(db *badger.DB) (float32, error) {
-	var balance float32
+func getBalance(db *badger.DB) (int, error) {
+	var balance int
 	err := db.View(func(txn *badger.Txn) error {
 		item, err := txn.Get([]byte("balance"))
 		if err != nil {
 			return err
 		}
 		return item.Value(func(val []byte) error {
-			balance = math.Float32frombits(binary.LittleEndian.Uint32(val))
+			balance = int(binary.LittleEndian.Uint32(val))
 			return nil
 		})
 	})
 	return balance, err
+}
+
+func getBalanceHandler(c echo.Context) error {
+	balance, err := getBalance(db)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+	return c.JSON(http.StatusOK, map[string]int{"balance": balance})
+}
+
+func updateBalanceHandler(c echo.Context) error {
+	balanceStr := c.FormValue("balance")
+	balance, err := strconv.Atoi(balanceStr)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid balance value"})
+	}
+	if err = updateBalance(db, balance); err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+	return c.JSON(http.StatusOK, map[string]string{"message": "Balance updated successfully"})
 }
